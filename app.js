8c08613c7239638ef42efa67fe1b625b020b8ad2
Gabi Monteiro <contatogabimonteiro@gmail.com>
2022-05-18 15:52:01 +0200

    .env credentials

--- app.js
+++ app.js
@@ -1,51 +1,48 @@
-
-const express = require('express');
-const morgan = require('morgan'); //middleware
-const mongoose = require('mongoose'); //connect to DB
-const blogRoutes = require('./routes/blogRoutes'); //to connect with blog routes
+const dotenv = require("dotenv");
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan"); //middleware
+const mongoose = require("mongoose"); //connect to DB
+const blogRoutes = require("./routes/blogRoutes"); //to connect with blog routes
 //express app
 const app = express();
-require("dotenv").config();
 
 
 //connect to MongoDB
-const dbURI = "mongodb+srv://firstdb:test1234@cluster0.yrswz.mongodb.net/summer-blog?retryWrites=true&w=majority";
 mongoose
-    .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.MONGODB_URI,
+        
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
     .then((result) =>
         app.listen(3000, () =>
             console.log(" listenen in localhost 3000 and connect mongoodb")
         )
     )
 
-    .catch((err) => console.log(err));
-
+    .catch((err) => console.log("no connection"));
 
 //register view enginee
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
 
 //static files middleware - public css acess to browser
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-
-
+app.use(morgan("dev"));
 
 //routes
-app.get('/', (req, res) => {
-    res.redirect('/posts');
+app.get("/", (req, res) => {
+    res.redirect("/posts");
 });
 
 app.get("/about", (req, res) => {
-    res.render("about", {title: 'About'});
-
+    res.render("about", { title: "About" });
 });
 
 //connect to Blog routes *middleware
-app.use('/posts', blogRoutes);
+app.use("/posts", blogRoutes);
 
 //404 page - middleware
-app.use((req,res) => {
+app.use((req, res) => {
     res.status(404).render("404", { title: "Not Found" });
-})
+});
